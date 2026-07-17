c81f5db413b61ddc2ef3a3dc07c5b9b1924efc00
SirZompiGit <roby96dark@gmail.com>
2026-07-17 11:27:56 +0200

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,7 +7,7 @@ const env = (import.meta as any).env;
 const firebaseConfig = {
   apiKey: env.VITE_FIREBASE_API_KEY || "AIzaSyDummyKey-InserisciLaTuaVeraKey",
   authDomain: env.VITE_FIREBASE_AUTH_DOMAIN || "tuo-progetto.firebaseapp.com",
-  databaseURL: env.VITE_FIREBASE_DATABASE_URL || `https://${env.VITE_FIREBASE_PROJECT_ID || "tuo-progetto"}-default-rtdb.europe-west1.firebasedatabase.app`,
+  databaseURL: env.VITE_FIREBASE_DATABASE_URL || `https://${env.VITE_FIREBASE_PROJECT_ID || "tuo-progetto"}-default-rtdb.firebaseio.com`,
   projectId: env.VITE_FIREBASE_PROJECT_ID || "tuo-progetto",
   storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET || "tuo-progetto.appspot.com",
   messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID || "123456789",
